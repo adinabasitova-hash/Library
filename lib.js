a35a46579b73c7180b845abef868d33abd8924e7
Adina <basitovaa@bk.ru>
2026-05-19 16:55:06 +0500

    Display example books

--- lib.js
+++ lib.js
@@ -1,11 +1,11 @@
 const myLibrary = [];
 
-function Book(id, title, author, pages, isRead) {
+function Book(title, author, pages, isRead) {
     this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.isRaed = isRead;
+    this.isRead = isRead;
   }
 
 
@@ -13,3 +13,26 @@ function addBookToLibrary(title, author, pages, isRead){
   const book = new Book(title, author, pages, isRead);
   myLibrary.push(book);
 }
+
+addBookToLibrary("1984", "George Orwell", 352, false);
+addBookToLibrary("Harry Potter", "J.K Rowling", 400, true);
+addBookToLibrary("Clean Code", "Robert Martin", 450, false);
+
+function displayBooks(){
+  const container = document.getElementById('library');
+  container.innerHTML = '';
+
+  myLibrary.forEach(book=>{
+    const card = document.createElement("div");
+    card.dataset.id = book.id;
+    card.innerHTML = `
+    <h3>${book.title}</h3>
+    <h4>${book.author}</h4>
+    <p>${book.pages}</p>
+    <p>Read: ${book.isRead ? 'Yep!' : 'Not yet'}</p>
+      `
+       container.appendChild(card);
+       console.log(book);
+  })
+}
+displayBooks();
